cba628ed946c089bc774de269b56affa2d4930af
devMarco14 <jonghyeok.lim14@gmail.com>
2023-03-14 20:42:28 +0900

    nav 영역 반응형 레이아웃 적용

--- js/main.js
+++ js/main.js
@@ -105,12 +105,35 @@ searchCancelEl.addEventListener("click", function () {
 
 //
 window.addEventListener("resize", function () {
-  if (this.window.innerWidth <= 740) {
+  if (window.innerWidth <= 740) {
     headerEl.classList.remove("searching");
   } else {
     headerEl.classList.remove("searching--mobile");
   }
 });
+
+const navEl = document.querySelector("nav");
+const navMenuToggleEl = navEl.querySelector(".menu-toggler");
+const navMenuShadowEl = navEl.querySelector(".shadow");
+
+navMenuToggleEl.addEventListener("click", function () {
+  if (navEl.classList.contains("menuing")) {
+    hideNavMenu();
+  } else {
+    showNavMenu();
+  }
+});
+navEl.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
+navMenuShadowEl.addEventListener("click", hideNavMenu);
+window.addEventListener("click", hideNavMenu);
+function showNavMenu() {
+  navEl.classList.add("menuing");
+}
+function hideNavMenu() {
+  navEl.classList.remove("menuing");
+}
 
 // 요소의 가시성 관찰
 const io = new IntersectionObserver(function (entries) {
